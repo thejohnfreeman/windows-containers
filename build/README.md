5528903b574d07f6f0fc0556e153cd8934bc82b5
John Freeman <jfreeman08@gmail.com>
2021-09-02 19:05:17 -0500

    Add Dockerfile for build-cpp image

---
+++ build/README.md
@@ -0,0 +1,3 @@
+```shell
+docker build --isolation process --memory 2GB --build-arg version=20H2 .
+```
---
+++ build/install.ps1
@@ -0,0 +1,35 @@
+$installer = 'vs_buildtools.exe'
+$channel = 'vs2019.chman'
+Invoke-WebRequest -Uri https://aka.ms/vs/16/release/$installer -OutFile $installer
+Invoke-WebRequest -Uri https://aka.ms/vs/16/release/channel -OutFile $channel
+
+$installPath =  'C:\BuildTools'
+$workloads = @(
+  'Microsoft.VisualStudio.Workload.VCTools'
+)
+$bootstrapperArgumentList = @(
+  '--quiet', '--wait', '--norestart', '--nocache'
+  '--installPath', $installPath
+  '--channelUri', $channel
+  '--installChannelUri', $channel
+  '--includeRecommended'
+  @($workloads | ForEach-Object { "--add $_" })
+)
+
+$process = Start-Process -FilePath $installer -ArgumentList $bootstrapperArgumentList -Wait -PassThru
+
+$exitCode = $process.ExitCode
+Write-Host "returned" $exitCode
+
+if ($exitCode -ne 0 -and $exitCode -ne 3010)
+{
+    Write-Host "installation failed"
+    exit $exitCode
+}
+
+rm $installer $channel
+
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+
+scoop install python git
